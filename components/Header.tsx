0c6a9891dcc181e1d0f9047ad144f20212682417
coa3 <lovecoal928@gmail.com>
2022-12-26 04:04:05 +0900

    add header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,33 @@
 import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import * as Icon from 'react-bootstrap-icons'
 
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    // ヘッダースクロールアニメーション
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        // 必要なとき以外はイベントを切る
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
+            {/* ヘッダーロゴ */}
             <Image
                 src="/netflix-logo.png"
                 width={100}
@@ -12,6 +36,7 @@ const Header = () => {
                 className="cursor-pointer object-contain"
             />
 
+            {/* ページナビゲーション */}
             <ul className="hidden space-x-4 md:flex">
                 <li className="headerLink">Home</li>
                 <li className="headerLink">TV Shows</li>
@@ -20,7 +45,23 @@ const Header = () => {
                 <li className="headerLink">My list</li>
             </ul>
         </div>
-        <div></div>
+
+        {/* アカウントナビゲーション */}
+        <div className="flex items-center space-x-4 text-sm font-light">
+          <Icon.Search className="hidden h-6 w-6 sm:inline"/>
+          <p className="hidden lg:inline">Kids</p>
+          <Icon.BellFill className="h-6 w-6"/>
+
+          <Link href="/account">
+            <Image
+                src="/account-icon.png"
+                width={30}
+                height={30}
+                alt="account icon"
+                className="cursor-pointer rounded"
+            />
+          </Link>
+        </div>
     </header>
   )
 }
