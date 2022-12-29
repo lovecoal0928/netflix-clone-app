dd0c522718ae97d6e06e9cd7f55ed86c50c25740
coa3 <lovecoal928@gmail.com>
2022-12-29 21:43:59 +0900

    add utils and Api requests file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511]
+      lg:h-[140vh]'
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -14,6 +16,7 @@ const Home: NextPage = () => {
         <Header />
         <main> 
           {/* バナー */}
+          <Banner />
           <section>
             {/* 再生リスト */}
             {/* 再生リスト */}
