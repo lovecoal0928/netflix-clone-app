0b45e7b37a7e7cfba27d52ee7f441a1771d8df63
coa3 <lovecoal928@gmail.com>
2023-01-02 00:54:37 +0900

    fix modal scroll bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,8 +37,7 @@ const Home = ({
   if (loading) return null
 
   return (
-    <div className='relative h-screen bg-gradient-to-b
-      lg:h-[140vh]'
+    <div className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${showModal && "!h-screen overflow-hidden"}`}
     >
       <Head>
         <title>Home - Netflix</title>
